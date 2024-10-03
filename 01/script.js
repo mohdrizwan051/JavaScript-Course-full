d782617ee90218909a34f394853692179a1cf24a
Mohd Rizwan <mohdrizwan962107@gmail.com>
2024-10-03 22:11:47 +0530

    for voting

--- 01/script.js
+++ 01/script.js
@@ -2,4 +2,19 @@ console.log("this is from external js file");
 console.log("Hey There");
 console.log("mohammad Rizwann");
 
+/*var age1 = 29;
+ if(age1 >= 18) {
+    console.log("you can cote");
+    else {
+        console.log("you are not eligible");
+    } 
+  }  */
+
+var age = 18; 
+if(age >= 21) { 
+    console. log('You can vote in the election'); 
+}
+ else { 
+  console. log('You are too young to vote');
+ }
 
