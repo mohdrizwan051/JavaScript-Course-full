241a3860db975fe4bf8147f89592f5ab1c7ad329
Mohd Rizwan <mohdrizwan962107@gmail.com>
2024-10-05 00:18:45 +0530

    new function for adult

--- script9.js
+++ script9.js
@@ -4,6 +4,13 @@ const name = "mohammad rizwan";
 const dob = "01/01/2000";
 
 function SayName (){
-    console.log("my name is " + ' ' + name);
+    console.log(" MY NAME IS ${name}");
     
 }
+var age4 = 18; 
+if(age >= 21) { 
+    console. log('You can vote in the election'); 
+}
+ else { 
+  console. log('You are too young to vote');
+ }
